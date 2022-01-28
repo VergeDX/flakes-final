6943f1aa8373090ef99783e4cd0fdf936202e5f3
Vanilla <osu_Vanilla@126.com>
2022-01-28 22:05:11 +0800

    feat[home-manager]: import home-manager for user vanilla.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   inputs = {
     nixos.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = { self, ... }@inputs: with inputs;
     flake-utils.lib.eachDefaultSystem (system: {
       packages.nixosConfigurations."NixOS-RoT" = nixos.lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
+        modules = [ ./configuration.nix ]
+          ++ [ home-manager.nixosModules.home-manager ]
+          ++ [{ home-manager.users."vanilla" = import ./home.nix; }];
       };
     });
 }
