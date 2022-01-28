96ec46043cbe64d7bdf0de528016c4dc43d9d3e4
Vanilla <osu_Vanilla@126.com>
2022-01-28 21:34:03 +0800

    style[hardware]: Separate boot config to boot.nix.

--- configuration.nix
+++ configuration.nix
@@ -10,11 +10,9 @@
       # Include the results of the hardware scan.
       ./hardware/configuration.nix
       ./hardware/disks.nix
-    ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+      ./boot.nix
+    ];
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
