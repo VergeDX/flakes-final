3b3f88fbfceca0181895e426288392528f5de81d
Vanilla <osu_Vanilla@126.com>
2022-01-28 21:06:05 +0800

    style[disks]: Remove resident line break, keep big parantheses on.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -19,26 +19,23 @@
     "/boot" = { device = "/dev/disk/by-uuid/F80E-3594"; fsType = "vfat"; };
   };
 
-  fileSystems."/nix" =
-    {
-      device = "/dev/disk/by-uuid/41407c27-1829-4cc4-bf34-d4167dabe1e1";
-      fsType = "btrfs";
-      options = [ "subvol=nix" "noatime" "compress-force=zstd" ];
-    };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/41407c27-1829-4cc4-bf34-d4167dabe1e1";
+    fsType = "btrfs";
+    options = [ "subvol=nix" "noatime" "compress-force=zstd" ];
+  };
 
-  fileSystems."/persistent" =
-    {
-      device = "/dev/disk/by-uuid/41407c27-1829-4cc4-bf34-d4167dabe1e1";
-      fsType = "btrfs";
-      options = [ "subvol=persistent" "noatime" "compress-force=zstd" ];
-    };
+  fileSystems."/persistent" = {
+    device = "/dev/disk/by-uuid/41407c27-1829-4cc4-bf34-d4167dabe1e1";
+    fsType = "btrfs";
+    options = [ "subvol=persistent" "noatime" "compress-force=zstd" ];
+  };
 
-  fileSystems."/etc/nixos" =
-    {
-      device = "/persistent/persistent/Projects/flakes-final";
-      fsType = "none";
-      options = [ "bind" ];
-    };
+  fileSystems."/etc/nixos" = {
+    device = "/persistent/persistent/Projects/flakes-final";
+    fsType = "none";
+    options = [ "bind" ];
+  };
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
